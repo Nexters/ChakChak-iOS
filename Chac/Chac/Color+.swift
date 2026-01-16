885c0c8d37452c9138e74192f1df47e16aeb6ce1
wongbingg <wb277@naver.com>
2026-01-16 21:10:04 +0900

    [#1] feat: Color Extension 구현
    - 추후 디자인시스템 개발 시 사용

---
+++ Chac/Chac/Color+.swift
@@ -0,0 +1,24 @@
+//
+//  Color+.swift
+//  Chac
+//
+//  Created by 이원빈 on 1/13/26.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: UInt, opacity: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 8) & 0xff) / 255,
+            blue: Double((hex >> 0) & 0xff) / 255,
+            opacity: opacity
+        )
+    }
+}
+
+enum ColorPalette {
+    static let gray = Color(hex: 0xF2F2F2)
+}
