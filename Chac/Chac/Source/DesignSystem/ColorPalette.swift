5e9153aeab93f66100c30c291ca70de5810e2a8c
wongbingg <wb277@naver.com>
2026-01-31 12:29:34 +0900

    [#9] feat: ColorPalette 추가

---
+++ Chac/Chac/Source/DesignSystem/ColorPalette.swift
@@ -0,0 +1,27 @@
+//
+//  ColorPallette.swift
+//  Chac
+//
+//  Created by 이원빈 on 1/29/26.
+//
+
+import SwiftUI
+
+enum ColorPalette {
+    static let primary = Color(hex: 0x7346FF)
+    static let accent1 = Color(hex: 0x8A65FF)
+    static let accent2 = Color(hex: 0xA284FF)
+    static let accent3 = Color(hex: 0xB39BFF)
+    static let accent4 = Color(hex: 0xE3DAFF)
+    
+    static let title1 = Color(hex: 0xF6F6F6)
+    static let title2 = Color(hex: 0xDDDDDD)
+    static let title3 = Color(hex: 0xBBBBBB)
+    static let caption = Color(hex: 0x999999)
+    
+    static let btn1 = Color(hex: 0xFFFFFF)
+    static let btn2 = Color(hex: 0x4A309B)
+    
+    static let bg = Color(hex: 0x141519)
+    static let bg_popup = Color(hex: 0x1D1E23)
+}
