1d0b4be9d37df6517339e859a06e83072b75f12d
wongbingg <wb277@naver.com>
2026-02-06 21:02:49 +0900

    [#9] chore: 디자인 시스템 수정

--- Chac/Chac/Source/DesignSystem/ColorPalette.swift
+++ Chac/Chac/Source/DesignSystem/ColorPalette.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 enum ColorPalette {
     static let primary = Color(hex: 0x7346FF)
-    static let accent1 = Color(hex: 0x8A65FF)
-    static let accent2 = Color(hex: 0xA284FF)
-    static let accent3 = Color(hex: 0xB39BFF)
-    static let accent4 = Color(hex: 0xE3DAFF)
+    static let sub_01 = Color(hex: 0x8A65FF)
+    static let sub_02 = Color(hex: 0xA284FF)
+    static let sub_03 = Color(hex: 0xB39BFF)
+    static let sub_04 = Color(hex: 0xE3DAFF)
     
-    static let title1 = Color(hex: 0xF6F6F6)
-    static let title2 = Color(hex: 0xDDDDDD)
-    static let title3 = Color(hex: 0xBBBBBB)
-    static let caption = Color(hex: 0x999999)
+    static let disable = Color(hex: 0x646385)
+    static let background = Color(hex: 0x141519)
+    static let background_popup = Color(hex: 0x1D1E23)
     
-    static let btn1 = Color(hex: 0xFFFFFF)
-    static let btn2 = Color(hex: 0x4A309B)
+    static let text_01 = Color(hex: 0xF6F6F6)
+    static let text_02 = Color(hex: 0xDDDDDD)
+    static let text_03 = Color(hex: 0xBBBBBB)
+    static let text_04 = Color(hex: 0x999999)
+    static let text_btn_01 = Color(hex: 0xFFFFFF)
+    static let text_btn_02 = Color(hex: 0x4A309B)
+    static let text_btn_03 = Color(hex: 0x353245)
     
-    static let bg = Color(hex: 0x141519)
-    static let bg_popup = Color(hex: 0x1D1E23)
+    static let stroke_01 = Color(hex: 0x7346FF)
+    static let stroke_02 = Color(hex: 0x785BD3)
+    static let stroke_03 = Color(hex: 0xB299FF)
+    
+    static let white_80 = Color(hex: 0xFFFFFF).opacity(0.8)
+    static let white_40 = Color(hex: 0xFFFFFF).opacity(0.4)
+    static let white_5 = Color(hex: 0xFFFFFF).opacity(0.05)
+    static let black_80 = Color(hex: 0x000000).opacity(0.8)
+    static let black_70 = Color(hex: 0x000000).opacity(0.7)
+    static let black_40 = Color(hex: 0x000000).opacity(0.4)
+    
+    static let etc = Color(hex: 0x666666)
+    static let splash = Color(hex: 0x6B28E5)
 }
--- Chac/Chac/Source/DesignSystem/FontStyle.swift
+++ Chac/Chac/Source/DesignSystem/FontStyle.swift
@@ -19,31 +19,43 @@ enum Typeface {
     }
 }
 
-enum Scale {
-    case headline_h1, headline_h2, headline_h3
-    case sub_headline1, sub_headline2
-    case body_large, body_medium, body_small1, body_small2
+enum ChacScale {
+    case headline_01, headline_02
+    case title
+    case sub_title_01, sub_title_02, sub_title_03
+    case content_title
+    case body
     case caption
+    case btn, sub_btn
+    case number
+    case toast_body
+    case sub_number
+    case splash_name
 
-    func generateFont(_ typeface: Typeface) -> Font {
+    func generateFont() -> Font {
         switch self {
-        case .headline_h1:   return Font.custom("\(typeface.prefix)-Medium", size: 22)
-        case .headline_h2:   return Font.custom("\(typeface.prefix)-Medium", size: 20)
-        case .headline_h3:   return Font.custom("\(typeface.prefix)-Bold", size: 18)
-        case .sub_headline1: return Font.custom("\(typeface.prefix)-Bold", size: 16)
-        case .sub_headline2: return Font.custom("\(typeface.prefix)-Medium", size: 15)
-        case .body_large:    return Font.custom("\(typeface.prefix)-Regular", size: 16)
-        case .body_medium:   return Font.custom("\(typeface.prefix)-Regular", size: 15)
-        case .body_small1:   return Font.custom("\(typeface.prefix)-Regular", size: 14)
-        case .body_small2:   return Font.custom("\(typeface.prefix)-Regular", size: 13)
-        case .caption:       return Font.custom("\(typeface.prefix)-Regular", size: 12)
+        case .headline_01:   return Font.custom("Pretendard-SemiBold", size: 20)
+        case .headline_02:   return Font.custom("Pretendard-Bold", size: 18)
+        case .title:         return Font.custom("Pretendard-Medium", size: 20)
+        case .sub_title_01:  return Font.custom("Pretendard-Medium", size: 18)
+        case .sub_title_02:  return Font.custom("Pretendard-Bold", size: 16)
+        case .sub_title_03:  return Font.custom("Pretendard-Medium", size: 14)
+        case .content_title: return Font.custom("Pretendard-Medium", size: 16)
+        case .body:          return Font.custom("Pretendard-Regular", size: 15)
+        case .caption:       return Font.custom("Pretendard-Medium", size: 12)
+        case .btn:           return Font.custom("Pretendard-SemiBold", size: 16)
+        case .sub_btn:       return Font.custom("Pretendard-Bold", size: 14)
+        case .number:        return Font.custom("Montserrat-SemiBold", size: 14)
+        case .toast_body:    return Font.custom("Pretendard-Regular", size: 14)
+        case .sub_number:    return Font.custom("Montserrat-SemiBold", size: 13)
+        case .splash_name:   return Font.custom("Montserrat-SemiBold", size: 38)
         }
     }
 }
 
 extension View {
-    func font(typeFace: Typeface, scale: Scale) -> some View {
+    func chacFont(_ scale: ChacScale) -> some View {
         return self
-            .font(scale.generateFont(typeFace))
+            .font(scale.generateFont())
     }
 }
